676ad9bcbcf53e35073d71fbae55a1896d3218bf
Giuseppe Dotto <gppdtt@gmail.com>
2025-09-01 23:44:31 +0200

    implemented post creation

--- src/classes/PostsManager.ts
+++ src/classes/PostsManager.ts
@@ -1,27 +1,6 @@
-import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
+import { collection, doc, getDocs, setDoc, updateDoc, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { IPost, Post } from "./Post";
-
-const firstPost = `
-Let's kick off this new adventure! The keywords which I would like to start with are #collaboration, #computational #design #technology and, like is easily guessable by looking at the page's name, #BIM.
-
-:::center{.image-small}
-
-![sssmoking](https://media2.giphy.com/media/12KDk0YabBSJHy/giphy.gif)
-
-:::
-
-> **The goal** is to create this sort of new platform where all the professionals who are using the Macro4BIM's tools can gather more information and get in touch with each other.
-
-This will allow to better refine and improve some existing tools as well as opening new and interesting topics that can stand as the spark which will let a new #macro born!
-
-**The formula** can work easily: it is just about to turn on some interesting chat under the blog if you want to discuss the post's topic. Otherwise, for the off-topic, there will always be availability texting at newtopic@macro4bim.com
-
-I am really thrilled by this M4B adventure, 
-hope you are as well (or will be)!
-
-Let's this Macro4BIM begin!!!
-`;
 
 export class PostsManager {
   posts: Post[];
@@ -30,11 +9,6 @@ export class PostsManager {
   constructor(posts: Post[]) {
     this.posts = posts;
     this.tags = this.tagsFromPosts();
-    this.createPlaceholderPost();
-  }
-
-  createPlaceholderPost() {
-    this.addPost({ title: "Hello World!", content: firstPost });
   }
 
   postBySlug(slug: string) {
@@ -53,7 +27,20 @@ export class PostsManager {
     const newPost = new Post(data, data.slug || slug);
     this.posts.push(newPost);
     this.tags = this.tagsFromPosts();
+    this.writeDB(newPost);
     return newPost;
+  }
+
+  private async writeDB(post: Post) {
+    const docRef = doc(db, "posts", post.slug);
+    const alreadyExist = (await getDoc(docRef)).exists();
+    if (alreadyExist) {
+      console.error(
+        `POST NOT STORED IN THE DB.\nSLUG ALREADY EXISTING IN THE DATABASE: ${post.slug}`
+      );
+      return;
+    }
+    await setDoc(docRef, { ...post });
   }
 
   publishPost(slug: string) {
